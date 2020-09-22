c5b43a0e7dd8fc4a720487fbe3077f8163c8653c
Santhosh J <santhosh@almabase.com>
2020-09-22 18:30:21 +0530

    Updates

---
+++ precache-manifest.02ec511d01a1b2af38a6987e030d9f89.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d0cfa08ab4ea31a24a7cd1346fd8a0b",
+    "url": "/overwatch-heroes-react/index.html"
+  },
+  {
+    "revision": "1b467e3afb70e0a0d2c4",
+    "url": "/overwatch-heroes-react/static/css/2.16d76422.chunk.css"
+  },
+  {
+    "revision": "461173d0c3998ea28915",
+    "url": "/overwatch-heroes-react/static/css/main.c52d75a4.chunk.css"
+  },
+  {
+    "revision": "1b467e3afb70e0a0d2c4",
+    "url": "/overwatch-heroes-react/static/js/2.f43ea413.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/overwatch-heroes-react/static/js/2.f43ea413.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "461173d0c3998ea28915",
+    "url": "/overwatch-heroes-react/static/js/main.b15eef6f.chunk.js"
+  },
+  {
+    "revision": "e7e3f065cad25796e675",
+    "url": "/overwatch-heroes-react/static/js/runtime-main.86a9215b.js"
+  },
+  {
+    "revision": "76dd2bbeb3da4220076b0b0e2bb492c1",
+    "url": "/overwatch-heroes-react/static/media/hero-background.76dd2bbe.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/overwatch-heroes-react/static/media/logo.ee7cd8ed.svg"
+  }
+]);
