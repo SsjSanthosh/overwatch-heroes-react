d0a9996bcf8c4f7cfae8a751dfaf21104f014d94
Santhosh J <santhosh@almabase.com>
2020-09-22 21:45:38 +0530

    removed proxy and added baseurl

--- src/redux/heroActions.js
+++ src/redux/heroActions.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 import { GET_HEROES_API_PATH } from "../Components/endpoints";
+console.log("url", process.env.REACT_APP_BASE_API_URL);
+console.log("node", process.env.NODE_ENV);
+axios.defaults.baseURL = "https://overwatch-heroes-api.herokuapp.com/";
 export const getHero = (id) => async (dispatch) => {
   try {
-    const res = await axios.get("/api/v1/heroes/list/" + id + "/");
+    const res = await axios.get("api/v1/heroes/list/" + id + "/");
     dispatch({ type: "GET_HERO", payload: res.data });
   } catch (err) {
     dispatch({
